1295b5a7991b938f7ded2b41c6b436cfe75c0887
matt <1654568495@qq.com>
2024-03-04 01:34:51 +0800

    fffas

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
-  ],
-  base: '/foo/'
+  ]
   ,
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
